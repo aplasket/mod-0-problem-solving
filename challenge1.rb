1cdde8cf9fec70500581d582cf36d64a4095ca73
Ashley Plasket <ashley.plasket@gmail.com>
2023-02-24 14:25:27 -0500

    fix code in final solution

--- challenge1.rb
+++ challenge1.rb
@@ -3,7 +3,7 @@
 # that have exactly 4 characters.
 
 #overall goal:
-# print an array that only includes the elements that have exactly 4 characters
+# print only the elements that have exactly 4 characters
 
 #psueduocode
 # step 1 - declare an array of string elements including a mixture of elements that have less than, equal to, and more than 4
@@ -11,10 +11,11 @@
 # step 3 - write a if statement using the length method to only print elements that have exactly 4 characters
 
 #final solution:
+# this code will print "Fred" "Stef"
 students = ["Fred", "George", "Harry", "Ron", "Stef", "Ab"]
 
-students.each do |student|
+    students.each do |student|
     if student.length == 4
-        p student
+        p student 
     end
 end
